c741d845a4215afae1b969c823c811d27b190ad7
kimanikamau1 <kimanikamau971@gmail.com>
2024-12-15 05:55:34 -0800

    python Intro task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_results = number1 + number2
+diff_results = number1 - number2
+mult_results = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_results}")
+print(f"Subtraction of {number1} and {number2} is {diff_results}")
+print(f"Multiplication of {number1} and {number2} is {mult_results}")
